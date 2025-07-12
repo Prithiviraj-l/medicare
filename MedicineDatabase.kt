5a4f8ad7852784aa3e6dc7597ef1cd0c5409c6e6
PRITHIVIRAJ <prithiviraj350g@gmail.com>
2025-07-12 21:44:42 +0530

    Create MedicineDatabase.kt

---
+++ MedicineDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.medicare.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Medicine::class], version = 1, exportSchema = false)
+abstract class MedicineDatabase : RoomDatabase() {
+    abstract fun medicineDao(): MedicineDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: MedicineDatabase? = null
+
+        fun getDatabase(context: Context): MedicineDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MedicineDatabase::class.java,
+                    "medicine_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
